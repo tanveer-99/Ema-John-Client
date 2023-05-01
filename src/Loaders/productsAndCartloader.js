2f26bbdc1d35940352f0f64262e42e724f025e0f
Tanveer <tanvir3512@gmail.com>
2023-05-02 01:51:53 +0600

    sd

--- src/Loaders/productsAndCartloader.js
+++ src/Loaders/productsAndCartloader.js
@@ -1,7 +1,7 @@
 import { getShoppingCart } from "../utilities/fakedb";
 export const productsAndCartLoader = async ()=> {
     // get product data
-    const productData = await fetch('http://localhost:5000/products');
+    const productData = await fetch('http://localhost:8000/products');
     const {products} = await productData.json();
     // get cart data
     const savedCart = getShoppingCart();
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -14,7 +14,7 @@ const Shop = () => {
     const [size, setSize] = useState(10);
 
     useEffect(()=> {
-        const url = `http://localhost:5000/products?page=${page}&size=${size}`
+        const url = `http://localhost:8000/products?page=${page}&size=${size}`
         fetch(url)
         .then(res => res.json())
         .then(data => {
@@ -29,7 +29,7 @@ const Shop = () => {
         const storedCart = getShoppingCart();
         const savedCart = [];
         const ids = Object.keys(storedCart);
-        fetch('http://localhost:5000/productsByIds', {
+        fetch('http://localhost:8000/productsByIds', {
             method: 'POST',
             headers: {
                 'content-type': 'application/json'
